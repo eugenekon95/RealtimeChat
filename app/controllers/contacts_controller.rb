fcb77e8ff12a159f436ca971f447a24b59fa3fa0
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2026-03-06 16:20:07 +0200

    Reciprocal add/remove contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,35 +1,49 @@
 class ContactsController < ApplicationController
-  def index
-    @users = current_user.added_users
-    @groups = current_user.added_groups
-  end
+  before_action :set_contact, only: [:destroy]
 
   def create
     contact = User.find(params[:user_id])
 
-    if current_user.added_users.exists?(contact.id)
-      redirect_to user_contacts_path(current_user), alert: "#{contact.email} is already in your contacts."
+    if already_contact?(contact)
+      redirect_to user_contacts_path(current_user),
+                  alert: "#{contact.email} is already in your contacts."
     else
-      current_user.added_users << contact
-      contact.added_users << current_user # Optional: Make the association reciprocal
+      add_contact(contact)
 
-      redirect_to user_contacts_path(current_user), notice: "#{contact.email} was successfully added to your contacts."
+      redirect_to user_contacts_path(current_user),
+                  notice: "#{contact.email} was successfully added to your contacts."
     end
   end
 
   def destroy
+    if already_contact?(@contact)
+      remove_contact(@contact)
 
-    contact = User.find(params[:id])
-
-    if current_user.added_users.exists?(contact.id)
-      current_user.added_users.delete(contact)
-      # Optionally make the removal reciprocal
-      contact.added_users.delete(current_user)
-
-
-      redirect_to user_contacts_path(current_user), notice: "#{contact.email} was successfully removed from your contacts."
+      redirect_to user_contacts_path(current_user),
+                  notice: "#{@contact.email} was successfully removed from your contacts."
     else
-      redirect_to user_contacts_path(current_user), alert: "#{contact.email} is not in your contacts."
+      redirect_to user_contacts_path(current_user),
+                  alert: "#{@contact.email} is not in your contacts."
     end
+  end
+
+  private
+
+  def set_contact
+    @contact = User.find(params[:id])
+  end
+
+  def already_contact?(user)
+    current_user.added_users.exists?(user.id)
+  end
+
+  def add_contact(user)
+    current_user.added_users << user unless already_contact?(user)
+    user.added_users << current_user unless user.added_users.exists?(current_user.id)
+  end
+
+  def remove_contact(user)
+    current_user.added_users.delete(user)
+    user.added_users.delete(current_user)
   end
 end
